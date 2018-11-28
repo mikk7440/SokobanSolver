4462d9778d7a37a6369e522ed3f4f2bc86cc8758
mikk7440 <miols15@student.sdu.dk>
2018-11-28 03:37:40 +0100

    Fixt copy.deepcopy + Q

--- solve.py
+++ solve.py
@@ -1,71 +1,3 @@
-######### File manipulation ###############
-f =open("simpleMap.txt","r")
-Data = f.readlines()
-Data = [x.strip('\n') for x in Data]
-f.close()
-
-########### Get meta info ################
-info= Data[0].split(" ")
-width = int(info[0])
-height = info[1]
-NumDiamonds = info[2]
-
-Data.pop(0) # pop meta info out.
-
-###########  Print Map ###################
-for x in range(len(Data)):
-    print Data[x]
-
-###############   Mapping    ######################################
-# M = robot start      J = Diamond     G = goal/dimond destination
-# . = Road             X = wall        " " = no map
-isRoad='.'
-RoadAdjencyList = [[]for i in range(len(Data)) ]
-isDiamond = 'J'
-Diamonds = []
-isGoal = 'G'
-Goals = []
-isRobotStart = 'M'
-isWall = 'X'
-RobotStart = []
-list = []
-
-# Check if walkaable field or X, save Road, Diammond etc.
-for i in range(len(Data)):
-    for j in range(len(Data[i])):
-        if  Data[i][j] == isRoad:
-            RoadAdjencyList[i].append(j)
-    #        list.append(j)
-        elif Data[i][j] == isDiamond:
-            Diamonds.append([i,j])
-            RoadAdjencyList[i].append(j)
-        elif Data[i][j] == isGoal:
-            Goals.append([i,j])
-            RoadAdjencyList[i].append(j)
-        elif Data[i][j] == isRobotStart:
-            RobotStart.append(i)
-            RobotStart.append(j)
-            RoadAdjencyList[i].append(j)
-    #if len(list)!= 0 :
-    #    RoadAdjencyList[i].append(list)
-    #list = []
-
-
-########## Print lists ############################
-print("\nAdjencylis:\n")
-print(RoadAdjencyList)
-
-print("\nDiamonds:\n")
-print(Diamonds)
-
-print("\nGoals:\n")
-print(Goals)
-
-print("\nRobotStart:\n")
-print(RobotStart)
-
-print("______________________________")
-
 ############# Class for search queue ###############
 import heapq
 
@@ -83,44 +15,58 @@ class PQueue:
         return heapq.heappop(self.elements)[1]
 
 ############# Game as a class ####################
+import copy
 class State(object):
     Road = []
     Diamonds = []
     Goals = []
     Player = []
     width=0
+    lastposition = (0,0)
     # The class "constructor" - It's actually an initializer
-    def __init__(self, Road, Diamonds, Goals, Player, width):
+    def __init__(self, Road, Diamonds, Goals, Player, width,lastposition):
         self.Road = Road
         self.Diamonds = Diamonds
         self.Goals = Goals
         self.Player = Player
         self.width= width
+        self.lastposition = lastposition
+
     def getPlayer(self):
         return tuple(self.Player)
+    def getDiamonds(self):
+        tupleD=()
+        for x in self.Diamonds:
+            tupleD = tuple(x) + tupleD
+        return tupleD
+    def getGoals(self):
+        tupleD=()
+        for x in self.Goals:
+            tupleD = tuple(x) + tupleD
+        return tupleD
 
     def getNeigboorsNotLast(self,oldPosition):
         currentN = self.neigboors()
         currentN.remove(oldPosition)
         return currentN
-
     def neigboors(self):                               # Neeed to reverse moves!=!
         neigboors = []
+        y = copy.copy(self.Player[0])
+        x = copy.copy(self.Player[1])
 
         if self.CheckUp():
-            neigboors.append(((self.Player[0]-1,self.Player[1]),"up"))
+            neigboors.append(((y-1,x),"up"))
 
         if self.CheckDown():
-            neigboors.append(((self.Player[0]+1,self.Player[1]),"down"))
+            neigboors.append(((y+1,x),"down"))
 
         if self.CheckLeft():
-            neigboors.append(((self.Player[0],self.Player[1]-1),"left"))
+            neigboors.append(((y,x-1),"left"))
 
         if self.CheckRight():
-            neigboors.append(((self.Player[0],self.Player[1]+1),"right"))
+            neigboors.append(((y,x+1),"right"))
 
         return neigboors
-
     def PMap (self):
         line = ""
         R = 0 # Road
@@ -165,7 +111,6 @@ class State(object):
             supR=0
             #print R
         return;
-
     def up (self):
         row = self.Player[0]-1
         col = self.Player[1]
@@ -176,14 +121,14 @@ class State(object):
             if [row-1,col] in self.Diamonds:
                 DoubleDimond=True
             else:#no dimond besides dimond
-                for i in range(len(Diamonds)): #Find dimond and check if its movable
-                    if [row,col] == Diamonds[i]:
-                        for j in range(len(Diamonds)):
-                            if [row-1,col] == Diamonds[j]:
+                for i in range(len(self.Diamonds)): #Find dimond and check if its movable
+                    if [row,col] == self.Diamonds[i]:
+                        for j in range(len(self.Diamonds)):
+                            if [row-1,col] == self.Diamonds[j]:
                                 DimondBlocked=True
                         if col in self.Road[row-1]: #Road pice after moved.... NO EXCEPTIon CATCH
                             if not DimondBlocked: # check for not blocked
-                                Diamonds[i]=[row-1,col]
+                                self.Diamonds[i]=[row-1,col]
                         else: #There is a isWall
                             DimondBlocked = True
 
@@ -191,6 +136,7 @@ class State(object):
             self.Player[0]=row
 
         if self.Player[0] == row:
+            self.lastposition = self.getPlayer()
             return True;
         else:
             return False;
@@ -204,14 +150,14 @@ class State(object):
             if [row+1,col] in self.Diamonds:
                 DoubleDimond=True
             else:#no dimond besides dimond
-                for i in range(len(Diamonds)): #Find dimond and check if its movable
-                    if [row,col] == Diamonds[i]:
-                        for j in range(len(Diamonds)):
-                            if [row+1,col] == Diamonds[j]:
+                for i in range(len(self.Diamonds)): #Find dimond and check if its movable
+                    if [row,col] == self.Diamonds[i]:
+                        for j in range(len(self.Diamonds)):
+                            if [row+1,col] == self.Diamonds[j]:
                                 DimondBlocked=True
                         if col in self.Road[row+1]: #Road pice after moved.... NO EXCEPTIon CATCH
                             if not DimondBlocked: # check for not blocked
-                                Diamonds[i]=[row+1,col]
+                                self.Diamonds[i]=[row+1,col]
                         else: #There is a isWall
                             DimondBlocked = True
 
@@ -219,6 +165,7 @@ class State(object):
             self.Player[0]=row
 
         if self.Player[0] == row:
+            self.lastposition = self.getPlayer()
             return True;
         else:
             return False;
@@ -232,14 +179,14 @@ class State(object):
             if [row,col+1] in self.Diamonds: #Dimond next to diamond
                 DoubleDimond=True
             else:#no dimond besides dimond
-                for i in range(len(Diamonds)): #Find dimond and check if its movable
-                    if [row,col] == Diamonds[i]:
-                        for j in range(len(Diamonds)):
-                            if [row,col+1] == Diamonds[j]:
+                for i in range(len(self.Diamonds)): #Find dimond and check if its movable
+                    if [row,col] == self.Diamonds[i]:
+                        for j in range(len(self.Diamonds)):
+                            if [row,col+1] == self.Diamonds[j]:
                                 DimondBlocked=True
                         if col+1 in self.Road[row]: #Road pice after moved
                             if not DimondBlocked: # check for not blocked
-                                Diamonds[i]=[row,col+1]
+                                self.Diamonds[i]=[row,col+1] #Move
                         else: #There is a isWall
                             DimondBlocked = True
 
@@ -247,6 +194,7 @@ class State(object):
             self.Player[1]=col
 
         if self.Player[1] == col:
+            self.lastposition = self.getPlayer()
             return True;
         else:
             return False;
@@ -257,18 +205,17 @@ class State(object):
         DimondBlocked=False
 
         if [row,col] in self.Diamonds:
-            # move Diamond
             if [row,col-1] in self.Diamonds:
                 DoubleDimond=True
             else:#no dimond besides dimond
-                for i in range(len(Diamonds)): #Find dimond and check if its movable
-                    if [row,col] == Diamonds[i]:
-                        for j in range(len(Diamonds)):
-                            if [row,col-1] == Diamonds[j]:
+                for i in range(len(self.Diamonds)): #Find dimond and check if its movable
+                    if [row,col] == self.Diamonds[i]:
+                        for j in range(len(self.Diamonds)):
+                            if [row,col-1] == self.Diamonds[j]:
                                 DimondBlocked=True
                         if col-1 in self.Road[row]: #Road pice after moved
                             if not DimondBlocked: # check for not blocked
-                                Diamonds[i]=[row,col-1]
+                                self.Diamonds[i]=[row,col-1]
                         else: #There is a isWall
                             DimondBlocked = True
 
@@ -276,6 +223,7 @@ class State(object):
             self.Player[1]=col
 
         if self.Player[1] == col:
+            self.lastposition = self.getPlayer()
             return True;
         else:
             return False;
@@ -403,66 +351,40 @@ class State(object):
         else:
             return False;
     def SaveState (self):
-        return init_State(self.Road,self.Diamonds,self.Goals,self.Player,self.width)
+        return init_State( self.Road ,copy.deepcopy(self.Diamonds),self.Goals,copy.deepcopy(self.Player) ,self.width,copy.deepcopy(self.lastposition))
 
-def init_State( Road, Diamonds, Goals, Player, width):
-    state = State(Road, Diamonds, Goals, Player, width)
+def init_State( Road, Diamonds, Goals, Player, width,lastposition):
+    state = State(Road, Diamonds, Goals, Player, width,lastposition)
     return state
 
-game = init_State(RoadAdjencyList,Diamonds,Goals,RobotStart,width)
+def getPath(came_from, goal):
+    current = goal
+    path = []
+    while current != None:
+        path.append(current)
+        current = came_from[current]
+    return path
 
-######################## move #########################
-#
-# game.left()
-# # print game.Player
-# game.PMap()
-# game.up()
-# # print game.Player
-# game.PMap()
-# game.up()
-# # print game.Player
-# game.PMap()
-# game.right()
-# game.PMap() #!!! ERROR Diamond through wall!=!
-# print "=======================================\n"
-# print "=======================================\n"
-###################### Search ###############################
-def dsearch(game):
-    graph = tuple(self.Road)
-    start = tuple(self.Player)
-    goal = tuple(goalToT)
-    frontier = PQueue()
-    frontier.put(self,0) #Item, prioty
-    print "Starting in: ", start
-    came_from = {}
-    cost_so_far={}
-    came_from[start] = 0
-    cost_so_far[start]= 0
+########### H
+def H(Diamonds, Goals):
+    D = [] # Y1,X1
+    G = [] # Y2,X2
+    a=0
 
-    NewGame = init_State(self.Road,self.Diamonds,self.Goals,self.Player,self.width)
+    for i in range(len(Diamonds)-1):
+        D.append([Diamonds[i],Diamonds[i+1]])
+        G.append([Goals[i],Goals[i+1]])
 
+    for i in range(len(Diamonds)):
+        a = abs(Diamonds[i] - Goals[i]) + a
+    print "TXT: ", a
+    print "TXT: ", D
+    print "TXT: ", G
+    return a
 
-    while not frontier.empty():
-        current = frontier.get()
-        print "Current: ", current.PMap()
-
-        if current ==  goal:
-            print "Goal: ", current, " == ", goal
-            break
-
-        print "Print neigboors: ", self.neigboors()
-        for next in self.neigboors():
-
-            new_cost = cost_so_far[current] + 1
-            if next not in cost_so_far or new_cost < cost_so_far[next]:
-                cost_so_far[next] = new_cost
-                priority = new_cost
-                frontier.put(next,priority) #Item, priority
-                came_from[next] = current
-
-    return came_from, cost_so_far
-
+######################## Search #########################
 def justSearch(game):
+    print "\n===== Search start =========\n"
     #nGoals
     goals = game.Goals
     # Init Priot Queue
@@ -470,54 +392,176 @@ def justSearch(game):
     Q.put(game,0)   # Item, prioty
     # Init path and cost dictonaries
     came_from  = {} # Create dictonary
-    cost_so_far= {}
-    came_from[game.getPlayer()] = 0 # tuple player position
-    cost_so_far[game.getPlayer()]= 0
-    gamelist=[]
-
+    cost_so_far = {}
+    pseudoState=(game.getPlayer(),game.getDiamonds())
+    came_from[ pseudoState ]  = None # tuple player & diamonds position
+    cost_so_far[game.getPlayer()] = 0
 
     while not Q.empty():
         currentState = Q.get()
         print "\nCurrentState map:"
         currentState.PMap()
+        print "\nChecking diamonds == goals: \n", currentState.Diamonds , " == ", goals
+        if currentState.Diamonds == goals:    # Need to implement sevelreal goals
+            print "------------ Yes, congratz!------------"
+            with open("path.txt", "w+") as pathFile:
+                pathFile.write("Came from\n")
+                pathFile.write(str(getPath(came_from,(currentState.getPlayer(),currentState.getGoals() ) )))
+                pathFile.write("\nCost:\n")
+                pathFile.write(str(cost_so_far[currentState.getPlayer()]))
+            break
+        print "False."
+        print "\nCurrentState neigboors: \n", currentState.neigboors()
+        lastpseudoState = (copy.deepcopy(currentState.getPlayer()),copy.deepcopy(currentState.getDiamonds()) )
+        for next in currentState.neigboors():
+            print "     Current positin: ", currentState.getPlayer()                           #TODO: Check that next state is not the same as before, unless a dimond is moved.
+            print "     Child position: ", next[0], " Direction: ", next[1]
+            Direction = next[1]
+            NewGame = currentState.SaveState()
+            new_cost = cost_so_far[currentState.lastposition] +1
+            # Direction control
+            if Direction == "up":
+                print "     Went up"
+                NewGame.up()
+            elif Direction == "down":
+                print "     Went down"
+                NewGame.down()
+            elif Direction == "right":
+                print "     Went right"
+                NewGame.right()
+            elif Direction == "left":
+                print "     Went left"
+                NewGame.left()
+            else:
+                print "!! ERROR 01: next[1] was a Unvalid direction !!"
 
-    print "\nIs diamonds: ", currentState.Diamonds , " == ", goals
-    if currentState.Diamonds == goals:    # Need to implement sevelreal goals
-        print "Yes, congratz!"
-        pass
+            if NewGame.getDiamonds() == NewGame.getPlayer():
+                print "Went right ERROR: NewGame diamonds: ", NewGame.getDiamonds(), " and ", NewGame.getPlayer()
+                print "=============================================================================================================="
+                NewGame.PMap()
 
-    print "\nCurrentState neigboors: ", currentState.neigboors()
-    for next in currentState.neigboors():
-        print "     Child position: ", next[0], " Direction: ", next[1]
-        Direction = next[1]
-        NewGame = currentState.SaveState()
-        if Direction == "up":
-            print "     Went up"
-        elif Direction == "down":
-            print "     Went down"
-        elif Direction == "right":
-            print "     Went right"
-        elif Direction == "left":
-            print "     Went left"
-        else:
-            print "ERROR 01: next[1] was a Unvalid direction"
+            print "     == New state: =="
+            NewGame.PMap()
+            pseudoState=(NewGame.getPlayer(),NewGame.getDiamonds())
 
-
+            print "\n     New state pseudoState: ", pseudoState
+            print "     Came_from: ",lastpseudoState
+            # print"     ",came_from
+            if pseudoState not in came_from or new_cost < cost_so_far[NewGame.getPlayer()]:
+                cost_so_far[NewGame.getPlayer()] = new_cost
+                priority = new_cost + H(NewGame.getDiamonds(),NewGame.getGoals())
+                print "          Putted in to Q: ", NewGame.getPlayer()
+                Q.put(NewGame,priority) #Item, priority
+                came_from[pseudoState] = lastpseudoState
+            else:
+                print "          Already in Q, Pseudostate :", pseudoState
+            del NewGame
     print "\n===== Search finish =========\n"
-    return came_from
+    return came_from, cost_so_far
 
-print justSearch(game)
+############## Global sh* ##########
+## File manipulation ######
+f =open("simpleMap.txt","r")
+Data = f.readlines()
+Data = [x.strip('\n') for x in Data]
+f.close()
 
-print "\nTO do:"
-print "* Implent move in search to get new neighboors"
-print "* Implent move in search to get new neighboors"
-#Came,Cost = game.dsearch([6,3])
-#print "Came: ", Came
-#print "Cost: ", Cost
+########### Get meta info ################
+info= Data[0].split(" ")
+width = int(info[0])
+height = info[1]
+NumDiamonds = info[2]
 
-#OutPut
-#L=left
-#R=right
-#F=front
-#B=back
-#['']
+Data.pop(0) # pop meta info out.
+
+###########  Print Map ###################
+for x in range(len(Data)):
+    print Data[x]
+
+###############   Mapping    ######################################
+# M = robot start      J = Diamond     G = goal/dimond destination
+# . = Road             X = wall        " " = no map
+isRoad='.'
+RoadAdjencyList = [[]for i in range(len(Data)) ]
+isDiamond = 'J'
+Diamonds = []
+isGoal = 'G'
+Goals = []
+isRobotStart = 'M'
+isWall = 'X'
+RobotStart = []
+list = []
+
+# Check if walkaable field or X, save Road, Diammond etc.
+for i in range(len(Data)):
+    for j in range(len(Data[i])):
+        if  Data[i][j] == isRoad:
+            RoadAdjencyList[i].append(j)
+    #        list.append(j)
+        elif Data[i][j] == isDiamond:
+            Diamonds.append([i,j])
+            RoadAdjencyList[i].append(j)
+        elif Data[i][j] == isGoal:
+            Goals.append([i,j])
+            RoadAdjencyList[i].append(j)
+        elif Data[i][j] == isRobotStart:
+            RobotStart.append(i)
+            RobotStart.append(j)
+            RoadAdjencyList[i].append(j)
+    #if len(list)!= 0 :
+    #    RoadAdjencyList[i].append(list)
+    #list = []
+
+
+########## Print lists ############################
+print("\nAdjencylis:\n")
+print(RoadAdjencyList)
+
+print("\nDiamonds:\n")
+print(Diamonds)
+
+print("\nGoals:\n")
+print(Goals)
+
+print("\nRobotStart:\n")
+print(RobotStart)
+
+print("______________________________")
+
+
+
+def main():
+    game = init_State(RoadAdjencyList,Diamonds,Goals,RobotStart,width,tuple(RobotStart))
+    ######################## Test #########################
+
+
+    came, cost = justSearch(game)
+
+
+    # print "came: ", came
+    # print "cost: ", cost
+
+    #print "Path: \n", getPath(came,None,)
+
+    ##Solution
+    # game.down()
+    # game.PMap()
+    # print game.neigboors()
+    # game.right()
+    # game.PMap()
+
+
+    print "\nTO do:"
+     print "* Make heuristik improvement."
+    # print "* game.up in dsearch check if valid"
+    # print "* Moved a dimond last time"
+
+    #OutPut
+    #L=left
+    #R=right
+    #F=front
+    #B=back
+    #['']
+
+if __name__ == '__main__':
+    main()
